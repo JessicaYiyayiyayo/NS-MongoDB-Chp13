5dd81803063dc7e5d74ea62b3dcca08709972d11
Jessica J.Y.YI <96872245+JessicaYiyayiyayo@users.noreply.github.com>
2022-08-14 23:42:52 +0800

    Delete middleware directory

--- middleware/redirectIfAuthenticatedMiddleware.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req, res, next) => {
-    if (req.session._id) {
-        return res.redirect('/');
-    }
-    next()
-}
-
--- middleware/validateMiddleware.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req,res,next) =>{
-    if(req.body.title==""||req.body.body==""){
-        return res.redirect('/posts/new')
-    }
-    next()
-}
